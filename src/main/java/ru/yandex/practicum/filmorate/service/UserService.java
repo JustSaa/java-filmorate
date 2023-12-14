6b6a48a2897c6c1e8aa47cfa446585097942decb
Salavat <Justdrumm@yandex.ru>
2023-12-14 22:08:11 +0300

    refactor:CheckStyle solve

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -6,12 +6,20 @@ import java.util.*;
 
 public interface UserService {
     User addUser(User user);
+
     void deleteUser(int userId);
+
     Collection<User> getAllUsers();
+
     User updateUser(User user);
+
     List<User> getCommonFriends(int userId, int userFriendId);
+
     List<User> getFriends(int userId);
+
     User getUserById(int userId);
+
     void deleteFromFriends(int userId, int userFriendId);
+
     void addToFriend(int userId, int userFriendId);
 }
